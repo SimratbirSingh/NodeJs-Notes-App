68987afdf4a9ef28cbc12ad2b1843fe227c56e88
Simrat Singh <simratbirsehgal@gmail.com>
2023-02-01 00:29:49 +0530

    starting with notes app

---
+++ notes-app/app.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+// fs.writeFileSync("notes.txt", "This file was created by Node.Js");
+
+fs.writeFileSync("notes.txt", "My name is simrat.");
+
+fs.appendFileSync("notes.txt", " My age is 19");
