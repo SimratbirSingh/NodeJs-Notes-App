1f82f5eb75bc414dc2a2a1e534b8e4db19c70192
Simrat Singh <simratbirsehgal@gmail.com>
2023-02-04 23:42:01 +0530

    working with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,58 @@
-const validator = require("validator");
+const chalk = require("chalk");
+const { command, demandOption, string } = require("yargs");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const msg = getNotes();
+// customise yargs version
+yargs.version("1.1.0");
 
-console.log(msg);
+//Create Add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note ",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
 
-console.log(validator.isURL("https://www.npmjs.com/package/validator"));
+// Create Remove Command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note ",
+  handler: function () {
+    console.log("Removing a note!");
+  },
+});
+
+// Create list cpmmand
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: function () {
+    console.log("Listing out all notes");
+  },
+});
+
+// Creat read command
+yargs.command({
+  command: "read",
+  describe: "Read a note ",
+  handler: function () {
+    console.log("Reading a note!");
+  },
+});
+
+yargs.parse();
